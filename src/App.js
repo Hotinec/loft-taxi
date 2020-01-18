e1722fe5cbc24e90c23de1a2c3640c31d010bc82
Hotinec Artiom <hotinec2@gmail.com>
2020-01-18 16:19:50 +0300

    HW1 finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Navigation from './components/Navigation/Navigation.js';
+import Map from './components/Map/Map.js';
+import Login from './components/Login/Login.js';
+import Profile from './components/Profile/Profile.js';
+import Signup from './components/Signup/Signup.js';
+
+const ROUTES = {
+  login: (setRoute) => <Login setRoute={setRoute}/>,
+  map: () => <Map/>,
+  profile: () => <Profile/>,
+  signup: (setRoute) => <Signup setRoute={setRoute}/>
+};
 
 function App() {
+  const [route, setRoute] = useState("login");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App__header">
+        <Navigation 
+          routes={Object.keys(ROUTES)}
+          setRoute={setRoute}
+        />
       </header>
+      <section className="App__content">
+        {ROUTES[route](setRoute)}
+      </section>
     </div>
   );
 }
